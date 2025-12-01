29108164f23444612862fc953a02918fc9fe1e69
cseas002 <christoforosseas@gmail.com>
2025-12-01 19:14:31 +0100

    Fixed package name

--- install-local.sh
+++ install-local.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Install the package locally for testing
-PACKAGE_DIR="$HOME/Library/Application Support/typst/packages/preview/kth-thesis-proposal/0.1.0"
+PACKAGE_DIR="$HOME/Library/Application Support/typst/packages/preview/unofficial-kth-thesis-proposal/0.1.0"
 
-echo "Installing kth-thesis-proposal package locally..."
+echo "Installing unofficial-kth-thesis-proposal package locally..."
 mkdir -p "$PACKAGE_DIR"
 cp -r src "$PACKAGE_DIR/"
 cp -r template "$PACKAGE_DIR/"
